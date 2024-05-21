53840eaf251613e0ef602ae86005ee54428b28a0
Felix Schober <if23b087@technikum-wien.at>
2024-05-21 15:29:58 +0200

    4. Test Refactor: added spaceshipField and xCoordinate variables

--- string_calculator/src/alf.js
+++ string_calculator/src/alf.js
@@ -3,21 +3,23 @@ function Alf() {}
 Alf.prototype.findSpaceship = function(map) {
 	let failureText = "Spaceship lost forever."
 	let result = [ 0, 0 ]
+	let xCoordinate = 0;
 	let emptyField = '.'
+	let spaceshipField = 'X'
 	if (map.length === 0 || map === emptyField)
-		{
-			result = failureText
-		}
+	{
+		result = failureText
+	}
 
 	for (let i = 0; i < map.length; i++)
+	{
+		if (map[i] === spaceshipField)
 		{
-			if (map[i] === 'X')
-			{
-				break;
-			}
-
-			result[0]++;
+			break;
 		}
+
+		result[xCoordinate]++;
+	}
 
 	return result
 };
