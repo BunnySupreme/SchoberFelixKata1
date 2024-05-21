ba4048a7e95dbdd558647fc44f682ef9c6b8df36
Felix Schober <if23b087@technikum-wien.at>
2024-05-21 17:30:43 +0200

    12. test, Red

--- string_calculator/spec/alf_spec.js
+++ string_calculator/spec/alf_spec.js
@@ -46,4 +46,8 @@ describe('Alf', function() {
 		expect(alf.findSpaceship("..../....")).toEqual("Invalid input");
 	});
 
+    it('should return "Invalid input" for a string of ".....XX.."', function() {
+		expect(alf.findSpaceship(".....XX..")).toEqual("Invalid input");
+	});
+
 });
