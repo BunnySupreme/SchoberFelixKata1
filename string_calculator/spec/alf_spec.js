00bd9ee639255c7db252a67257433dd7dc050cb8
Felix Schober <if23b087@technikum-wien.at>
2024-05-21 15:05:00 +0200

    wrote first test case "empty string", is Red

--- string_calculator/spec/alf_spec.js
+++ string_calculator/spec/alf_spec.js
@@ -2,6 +2,8 @@ describe('Alf', function() {
 	beforeEach(function() {
 		alf = new Alf();
 	});
-
+    it('should return "Spaceship lost forever." for an empty string', function() {
+		expect(alf.findSpaceship('')).toEqual("Spaceship lost forever.");
+	});
 
 });
