384e55baa16b0351c887bc4f0d90449d5180deab
Felix Schober <if23b087@technikum-wien.at>
2024-05-21 15:20:45 +0200

    3. test refactor: simplified into one if, added emptyField variable

--- string_calculator/src/alf.js
+++ string_calculator/src/alf.js
@@ -3,13 +3,11 @@ function Alf() {}
 Alf.prototype.findSpaceship = function(map) {
 	let failureText = "Spaceship lost forever."
 	let result = [ 0, 0 ]
-	if (map.length === 0)
+	let emptyField = '.'
+	if (map.length === 0 || map === emptyField)
 		{
 			result = failureText
 		}
-	else if (map === '.')
-		{
-			result = failureText
-		}
+
 	return result
 };
